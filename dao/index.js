7e0d07641c2fdb1d196d39dae3a4c16f4f15e6ee
Luis <20193905@aloe.ulima.edu.pe>
2022-11-29 01:31:18 -0500

    a

---
+++ dao/index.js
@@ -0,0 +1,487 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+// postgres://<USUARIO>:<PASSWORD>@<URL_HOST_BD>:<PUERTO_BD>/<NOMBRE_BD>
+
+
+const CADENA_CONEXION = /*process.env.DATABASE_URL ||*/ 
+    "postgres://postgres:postgres@localhost:5433/postgres"
+    const sequelize = new Sequelize(CADENA_CONEXION)
+    /*const sequelize = new Sequelize(CADENA_CONEXION,{
+        dialectOptions : {
+            ssl : {
+                require : true,
+                rejectUnauthorized : false
+            }
+        }
+    })*/
+
+    const usuario = sequelize.define("usuario", {
+        id : {
+            primaryKey : true,
+            type : DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        nombre : {
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        apellido: {
+            type : DataTypes.STRING(200),
+            allowNull : true
+        },  
+        correo:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        contrasena:{
+            type: DataTypes.STRING(200),
+            allowNull : false
+        },
+        direccion: {
+            type : DataTypes.STRING(200),
+            allowNull : true
+        },
+        departamento: {
+            type : DataTypes.STRING(200),
+            allowNull : true
+        },
+        ciudad: {
+            type :  DataTypes.STRING(200),
+            allowNull : true
+        },
+        cod_post: {
+            type : DataTypes.STRING(200),
+            allowNull : true
+        },
+        telefono: {
+            type : DataTypes.INTEGER,
+            allowNull : false
+        }
+
+    }, {
+        timestamps : false,
+        freezeTableName : true
+    })
+    const producto = sequelize.define("producto",{
+        id : {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        nombre:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        precio: {
+            type : DataTypes.INTEGER,
+            allowNull : false
+        },  
+        descripcion: {
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        categoria:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    })
+
+    const orden = sequelize.define("orden",{
+        id : {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        monto:{
+            type : DataTypes.INTEGER,
+            allowNull : false
+        },
+        direccion: {
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        fecha: {
+            type : DataTypes.DATE,
+            allowNull : false
+        },
+        usuario_id:{
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4,
+            allowNull : false
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    })
+    const orden_producto = sequelize.define("orden_producto",{
+        id : {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        orden_id : {
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        producto_id : {
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    })
+    const pc_armado = sequelize.define("pc_armado",{
+        id : {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        nombre: {
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        descripcion: {
+            type : DataTypes.STRING(200),
+            allowNull : false
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    })
+    const pc_armado_producto = sequelize.define("pc_armado_producto",{
+        id: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        pc_armado_id: {
+            
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        producto_id: {
+            
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        nombre: {
+            type : DataTypes.STRING(200),
+            
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    })
+    const reporte =  sequelize.define("reporte",{
+        id: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        correo:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        nombre:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        telefono:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        asunto:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        descripcion:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        usuario_id: {
+            
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    })
+
+    const resena =  sequelize.define("resena",{
+        id: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        },
+        puntaje:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        comentario:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        video:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        link:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        tipo_resena:{
+            type : DataTypes.STRING(200),
+            allowNull : false
+        },
+        usuario_id: {
+            
+            type: DataTypes.UUID,
+            defaultValue : Sequelize.UUIDV4
+        }
+    },
+    {
+        timestamps : false,
+        freezeTableName : true
+    })
+
+    //Relaciones
+    //reporte *-------->1 usuario
+    reporte.belongsTo(usuario,{
+        foreignKey : "usuario_id"
+    })
+    usuario.hasMany(reporte,{
+        foreignKey : "id"
+    })
+    //resena *-------->1 usuario
+    resena.belongsTo(usuario,{
+        foreignKey : "usuario_id"
+        })
+    usuario.hasMany(resena,{
+        foreignKey : "id"
+    })
+    //orden *-------->1 usuario
+    orden.belongsTo(usuario,{
+        foreignKey : "usuario_id"
+    })
+    usuario.hasMany(orden,{
+        foreignKey : "id"
+    })
+    //orden_producto *-------->1 orden
+    orden_producto.belongsTo(orden,{
+        foreignKey : "orden_id"
+    })
+    orden.hasMany(orden_producto,{
+        foreignKey : "id"
+    })
+    //orden_producto *-------->1 producto
+    orden_producto.belongsTo(producto,{
+        foreignKey : "producto_id"
+    })
+    producto.hasMany(orden_producto,{
+        foreignKey : "id"
+    })
+    //pc_armado_producto *-------->1 producto
+    pc_armado_producto.belongsTo(producto,{
+        foreignKey : "producto_id"
+    })
+    producto.hasMany(pc_armado_producto,{
+        foreignKey : "id"
+    })
+    //pc_armado_producto *-------->1 pc_armado
+    pc_armado_producto.belongsTo(pc_armado,{
+        foreignKey : "pc_armado_id"
+    })
+    pc_armado.hasMany(pc_armado_producto,{
+        foreignKey : "id"
+    })
+
+    module.exports = { usuario, producto, orden, orden_producto, pc_armado, pc_armado_producto, reporte, resena }
+/*
+const CADENA_CONEXION = 
+    "postgresql://evaluaciones:evaluaciones@localhost:5432/evaluacionesdb"
+
+const sequelize = new Sequelize(CADENA_CONEXION)
+
+const Carrera = sequelize.define("carrera", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    nombre : {
+        type : DataTypes.STRING(200),
+        allowNull : false
+    } 
+}, {
+    timestamps : false,
+    freezeTableName : true
+})
+
+const Curso = sequelize.define("curso", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    nombre : {
+        type : DataTypes.STRING(150),
+        allowNull : false
+    },
+    carrera_id : {
+        type : DataTypes.UUID,
+        allowNull : true
+    }
+}, {
+    timestamps : false,
+    freezeTableName : true
+})
+
+const Evaluacion = sequelize.define("evaluacion", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    nombre : {
+        type : DataTypes.STRING(200),
+        allowNull : false
+    },
+    fecha_registro : {
+        type : DataTypes.DATE,
+        allowNull : true
+    },
+    curso_id : {
+        type : DataTypes.UUID,
+        allowNull : false
+    },
+    ciclo_id : {
+        type : DataTypes.UUID,
+        allowNull : false
+    }
+}, {
+    timestamps : false,
+    freezeTableName : true
+})
+
+const Ciclo = sequelize.define("ciclo", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    nombre : {
+        type : DataTypes.STRING(200),
+        allowNull : false
+    }
+}, {
+    timestamps : false,
+    freezeTableName : true
+})
+
+const Estudiante = sequelize.define("estudiante", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    username : {
+        type : DataTypes.STRING(20),
+        allowNull : false
+    },
+    password : {
+        type : DataTypes.STRING(100),
+        allowNull : false
+    }
+}, {
+    timestamps : false,
+    freezeTableName : true
+})
+
+const Resolucion = sequelize.define("resolucion", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    estudiante_id : {
+        type : DataTypes.UUID,
+        allowNull : false        
+    },
+    evaluacion_id : {
+        type : DataTypes.UUID,
+        allowNull : false
+    },
+    fecha_envio : {
+        type : DataTypes.DATE,
+        allowNull : false
+    },
+    upvote : {
+        type : DataTypes.INTEGER
+    },
+    url : {
+        type : DataTypes.STRING(2048)
+    }
+}, {
+    timestamps : false,
+    freezeTableName : true
+})
+
+
+// Relaciones
+// Curso * <----> 1 Carrera
+Curso.belongsTo(Carrera, {
+    foreignKey : "carrera_id"
+})
+Carrera.hasMany(Curso, {
+    foreignKey : "id"
+})
+
+// Evaluacion * <----> 1 Curso
+Evaluacion.belongsTo(Curso, {
+    foreignKey : "curso_id"
+})
+Curso.hasMany(Evaluacion, {
+    foreignKey : "id"
+})
+
+// Evaluacion * <----> 1 Ciclo
+Evaluacion.belongsTo(Ciclo, {
+    foreignKey : "ciclo_id"
+})
+Ciclo.hasMany(Evaluacion, {
+    foreignKey : "id"
+})
+
+// Resolucion * <----> 1 Estudiante
+Resolucion.belongsTo(Estudiante, {
+    foreignKey : "estudiante_id"
+})
+Estudiante.hasMany(Resolucion, {
+    foreignKey : "id"
+})
+
+// Resolucion * <----> 1 Evaluacion
+Resolucion.belongsTo(Evaluacion, {
+    foreignKey : "evaluacion_id"
+})
+Evaluacion.hasMany(Resolucion, {
+    foreignKey : "id"
+})
+
+
+module.exports = {
+    Carrera, Curso, Ciclo, Evaluacion, Estudiante, Resolucion
+}
+*/
+
---
+++ index.js
@@ -0,0 +1,217 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const data = require("./test_data") // importamos data de test
+const { usuario, producto , orden , orden_producto, pc_armado, pc_armado_producto, reporte, resena} = require("./dao")
+
+/*const PUERTO = process.env.PORT || 4445*/
+const PUERTO = 4447
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended : true
+}))
+app.use(cors()) // politica CORS (cualquier origen) <---- TODO: cuidado!!!
+app.use(express.static("assets")) // <-- configuracion de contenido estatico
+
+
+app.post("/usuarios", async (req,resp) => {
+    const dataRequest = req.body
+    const nombre = dataRequest.nombre
+    const apellido = dataRequest.apellido
+    const correo = dataRequest.correo
+    const cod_post= dataRequest.cod_post
+    const telefono = dataRequest.telefono
+    const ciudad = dataRequest.ciudad
+    const departamento = dataRequest.departamento
+    const direccion = dataRequest.direccion
+    const contrasena = dataRequest.contrasena
+
+    await usuario.create({
+        nombre : nombre,
+        apellido : apellido,
+        correo : correo,
+        cod_post : cod_post,
+        telefono : telefono,
+        ciudad : ciudad,
+        departamento : departamento,
+        direccion : direccion,
+        contrasena : contrasena,
+    })
+
+    resp.send({
+        confirmar: "Registro exitoso"
+    })
+})
+app.post("/reporte", async (req,resp) => {
+    const dataRequest = req.body
+    const correo = dataRequest.correo
+    const nombre = dataRequest.nombre
+    const telefono = dataRequest.telefono
+    const asunto = dataRequest.asunto
+    const descripcion = dataRequest.descripcion
+    //se necesitaria esto? 
+    const usuario_id = dataRequest.usuario_id
+
+    
+    await reporte.create({
+        correo : correo,
+        nombre : nombre,
+        telefono : telefono,
+        asunto : asunto,
+        descripcion : descripcion,
+        usuario_id : usuario_id,
+    })  
+    resp.send({
+        confirmar: "Reporte enviado correctamente"
+    })
+})
+app.post("/resena", async (req,resp) => {
+    const dataRequest = req.body
+    const puntaje = dataRequest.puntaje
+    const comentario = dataRequest.comentario
+    const video = dataRequest.video
+    const link = dataRequest.link
+    const tipo_resena = dataRequest.tipo_resena
+    const usuario_id = dataRequest.usuario_id
+    await resena.create({
+        puntaje : puntaje,
+        comentario : comentario,
+        video : video,
+        link : link,
+        tipo_resena : tipo_resena,
+        usuario_id : usuario_id,
+    })
+    resp.send({
+        confirmar: "Reseña enviada correctamente"
+    })
+})
+
+app.get("/usuarios", async (req, resp) => {
+    const listausuario = await usuario.findAll()
+    resp.send(listausuario)
+})
+
+app.get("/productos", async (req, resp) => {
+        const pc_armado_id = req.query.pc_armado_producto
+        if(pc_armado_id == undefined || pc_armado_id == "-1"){
+            const listaproductos = await producto.findAll()
+            resp.send(listaproductos)
+        }
+        else{
+            const componentesFiltrados = await producto.findAll({
+                where : {pc_armado_id : pc_armado_id}})
+            resp.send(componentesFiltrados)
+        }
+})
+
+
+
+app.get("/pcarmadas", async (req, resp) => {
+    const listapcarmado = await pc_armado.findAll()
+    resp.send(listapcarmado)
+})
+app.get("/pcarm_produto", async (req, resp) => {
+    const listapcarm_produto = await pc_armado_producto.findAll()
+    resp.send(listapcarm_produto)
+    
+
+})
+app.get("/historial_compras", async (req, resp) => {
+    const listahistorial_compras = await orden_producto.findAll()
+    resp.send(listahistorial_compras)
+})
+
+app.get("/orden", async (req, resp) => {
+    const listaorden = await orden.findAll()
+    resp.send(listaorden)
+    })
+
+
+
+app.listen(PUERTO, () => {
+    console.log(`Servidor web iniciado en puerto ${PUERTO}`)
+})
+/*
+//1. Servicio que nos devuelva una lista de carreras
+// path: "/carreras" metodo: GET
+app.get("/carreras", async (req, resp) => {
+    const listaCarreras = await Carrera.findAll()
+
+    resp.send(listaCarreras)
+})
+
+
+//2. Servicio (endpoint) que nos devuelva una lista de cursos
+// path: "/cursos" metodo: GET
+// query parameter "/cursos?carrera=1"
+app.get("/cursos", async (req, resp) => {
+    const carreraId = req.query.carrera
+
+    if (carreraId == undefined || carreraId === "-1") {
+
+        const listaCursos = await Curso.findAll()
+
+        resp.send(listaCursos)
+    }else {
+        const cursosFiltrados = await Curso.findAll({
+            where : {
+                carrera_id : carreraId
+            }
+        })
+        resp.send(cursosFiltrados)
+    }
+
+})
+
+// 3. Endpoint para listar ciclos
+app.get("/ciclos", async (req, resp) => {
+    const listadoCiclos = await Ciclo.findAll()
+    resp.send(listadoCiclos)
+})
+
+// 4. Endpoint para listar evaluaciones
+// path: "/evaluaciones" metodo: GET
+// query parameter "/evaluaciones?curso=12312&ciclo=23523532"
+app.get("/evaluacion", async (req, resp) => {
+    const cursoId = req.query.curso
+    const cicloId = req.query.ciclo
+
+    if (cicloId == undefined || cicloId === "-1"){
+        // Caso que no se seleccione ciclo
+        const listadoEvaluaciones = await Evaluacion.findAll({
+            where : {
+                curso_id : cursoId
+            }
+        })
+        resp.send(listadoEvaluaciones)
+    }else {
+        // Caso que SI se seleccione ciclo
+        const listadoEvaluaciones = await Evaluacion.findAll({
+            where : {
+                curso_id : cursoId,
+                ciclo_id : cicloId
+            }
+        })
+        resp.send(listadoEvaluaciones)
+    }
+})
+
+// 5: Registro de resolucion de evaluacion
+// Recibir la data en el Cuerpo peticion HTTP (POST)
+// Request:
+// {
+//      estudiante_id : "22344523532",
+//      evaluacion_id : "22344523532",
+//      url : "http://blablac.com/archivo.zip",
+// }
+app.post("/resolucion", (req, resp) => {
+    
+})
+
+app.listen(PUERTO, () => {
+    console.log(`Servidor web iniciado en puerto ${PUERTO}`)
+})
+*/
